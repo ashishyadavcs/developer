8f149d2a45191a09a0af956a1a823b47fca04684
ashish221306 <ashish@#123>
2020-06-01 13:53:55 +0530

    mhdkuhfuir

---
+++ js/chat.js
@@ -0,0 +1,12 @@
+
+(function(w,d){
+function r(){
+var s=document.createElement('script');s.async=1;s.type='text/javascript';s.src='https://chatflow.io/static/1.2/chatflow.php';(d.body||d.head).appendChild(s);
+s.onload = function () {
+var options = {"channels":{"messenger":"hillyourself2019","whatsapp":"+917068513356"},"agentname":"Ashish Kumar","color":"#824FFD","position":"right","ctaText":"want to hire me ?"}
+cffunc = new chatflow();
+cffunc.loadcf('init', options);
+};
+}
+if(w.attachEvent){w.attachEvent('onload',r)}else{w.addEventListener('load',r,false)}
+})(window, document);
