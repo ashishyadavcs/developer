614ba7550acf7503498d281c784c405f9a0b021a
Ashish Kumar <ashishbhu221306@gmail.com>
2024-04-09 15:55:39 +0530

    fixed

--- js/route.js
+++ js/route.js
@@ -1,50 +1,40 @@
+angular.module("myapp", ["ngRoute"]).config([
+  "$routeProvider",
+  function ($routeProvider) {
+    $routeProvider
+      .when("/", {
+        templateUrl: "home.html",
+      })
 
-angular.module('myapp', ['ngRoute']).config(['$routeProvider',function($routeProvider)
-{
-$routeProvider
-.when('/', {
- templateUrl: 'home.html',
- 
-})
- 
- 
-.when('/services', {
-    templateUrl: 'services.html',
-   
-   })
-   .when('/portfolio', {
-    templateUrl: 'portfolio.html',
-    
-   })
-  .when('/top300-most-important-javascript-interview-questions', {
-    templateUrl: 'top300-most-important-javascript-interview-questions',
-   })
-    .when('/techskills', {
-    templateUrl: 'techskills.html',
-    
-   })
-   .when('/payment-terms', {
-    templateUrl: 'payment-terms.html',
-    
-   })
-   .when('/contact-us', {
-    templateUrl: 'contact-us.html',
-   })
-   .when('/aboutme', {
-    templateUrl: 'aboutme.html',
-    
-   })
-   .when('/ashishkumar', {
-    templateUrl: 'ashishkumar.html',
-    
-   })
-   .when('/hireme', {
-    templateUrl: 'hireme.html',
-    
-   })
-   .when('/website-designer-in-delhi', {
-    templateUrl: 'website-designer-in-delhi.html',
-    
-   })
- 
-}]); 
+      .when("/services", {
+        templateUrl: "services.html",
+      })
+      .when("/portfolio", {
+        templateUrl: "portfolio.html",
+      })
+      .when("/top300-most-important-javascript-interview-questions", {
+        templateUrl: "top300-most-important-javascript-interview-questions",
+      })
+      .when("/techskills", {
+        templateUrl: "techskills.html",
+      })
+      .when("/payment-terms", {
+        templateUrl: "payment-terms.html",
+      })
+      .when("/contact-us", {
+        templateUrl: "contact-us.html",
+      })
+      .when("/aboutme", {
+        templateUrl: "aboutme.html",
+      })
+      .when("/ashishkumar", {
+        templateUrl: "ashishkumar.html",
+      })
+      .when("/hireme", {
+        templateUrl: "hireme.html",
+      })
+      .when("/website-designer-in-delhi", {
+        templateUrl: "website-designer-in-delhi.html",
+      });
+  },
+]);
